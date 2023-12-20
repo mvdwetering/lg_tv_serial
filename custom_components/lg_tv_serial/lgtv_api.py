68215cc02f7b753ca288b432cde81616c6ca59d2
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2023-12-20 21:04:53 +0100

    Cleanup and format lgtv_api

--- custom_components/lg_tv_serial/lgtv_api.py
+++ custom_components/lg_tv_serial/lgtv_api.py
@@ -1,3 +1,4 @@
+"""LG TV API, this should really be in its own package, but not now"""
 import asyncio
 from dataclasses import dataclass
 from enum import IntEnum, unique
@@ -11,8 +12,8 @@ import serial_asyncio
 
 logger = logging.getLogger(__name__)
 
-END_MARKER = b'x'
-WEIRD_VALUE = 0xFF
+END_MARKER = b"x"
+
 
 @unique
 class RemoteKeyCode(IntEnum):
@@ -81,6 +82,7 @@ class RemoteKeyCode(IntEnum):
     APP = 0x9F
     TV_PC = 0x9B
 
+
 @unique
 class Input(IntEnum):
     DTV = 0x00
@@ -89,7 +91,7 @@ class Input(IntEnum):
     ISDB_CS1_JAPAN = 0x03
     ISDB_CS2_JAPAN = 0x04
     CATV = 0x11
-    AV1= 0x20
+    AV1 = 0x20
     AV2 = 0x21
     COMPONENT1 = 0x40
     COMPONENT2 = 0x41
@@ -99,6 +101,7 @@ class Input(IntEnum):
     HDMI3 = 0x92
     HDMI4 = 0x93
 
+
 @unique
 class Mode3D(IntEnum):
     ON = 0x00
@@ -106,6 +109,7 @@ class Mode3D(IntEnum):
     TO_2D = 0x02
     TO_3D = 0x03
     UNKNOWN = 0xFF
+
 
 @unique
 class Encoding3D(IntEnum):
@@ -120,50 +124,70 @@ class Encoding3D(IntEnum):
 
 @dataclass
 class Config3D:
-    mode:Mode3D
-    encoding:Encoding3D
-    right_to_left:bool
-    depth:int
+    mode: Mode3D
+    encoding: Encoding3D
+    right_to_left: bool
+    depth: int
+
 
 @dataclass
 class Response:
     command2: str
-    set_id:int
-    status_ok:bool
-    data0:int
-    data1:int|None = None
-    data2:int|None = None
-    data3:int|None = None
-    data4:int|None = None
-    data5:int|None = None    
+    set_id: int
+    status_ok: bool
+    data0: int
+    data1: int | None = None
+    data2: int | None = None
+    data3: int | None = None
+    data4: int | None = None
+    data5: int | None = None
 
-def build_command(command1, command2, set_id:int, data0:int, data1:int|None = None, data2:int|None = None, data3:int|None = None, data4:int|None = None, data5:int|None = None) -> bytes:
+
+def build_command(
+    command1,
+    command2,
+    set_id: int,
+    data0: int,
+    data1: int | None = None,
+    data2: int | None = None,
+    data3: int | None = None,
+    data4: int | None = None,
+    data5: int | None = None,
+) -> bytes:
     arguments = locals()
-    # print(arguments)
 
     command_string = f"{command1}{command2} {set_id:02X}"
     data_index = 0
     while arguments[f"data{data_index}"] is not None:
         data = arguments[f"data{data_index}"]
-        # print(data_index, data)
         command_string += f" {data:02X}"
         data_index += 1
     command_string += "\r"  # CR
     command = command_string.encode("ascii")
 
-    logging.debug("build_command string: %s", command_string)
-    logging.debug("build_command bytes: %s", command)
+    logger.debug("build_command string: %s" % command_string)
+    logger.debug("build_command bytes: %r" % command)
 
     return command
 
-def parse_response(reponse:bytearray) -> Response | None:
+
+def parse_response(reponse: bytearray) -> Response | None:
     try:
-        match = re.match(r"(?P<cmd2>.) (?P<set_id>\d\d) (?P<status>..)(?P<data>.+)", reponse.decode("ascii"))
+        match = re.match(
+            r"(?P<cmd2>.) (?P<set_id>\d\d) (?P<status>..)(?P<data>.+)",
+            reponse.decode("ascii"),
+        )
         if match is None:
-            logging.error("Could not match %s", reponse)
+            logger.error("Could not match %s", reponse)
             return None
 
-        logging.debug("DEBUG MATCH DATA: %s, %s, %s, %s", match.group("cmd2"), match.group("set_id"), match.group("status"), match.group("data"))
+        logger.debug(
+            "MATCH DATA: %s, %s, %s, %s",
+            match.group("cmd2"),
+            match.group("set_id"),
+            match.group("status"),
+            match.group("data"),
+        )
         cmd2 = match.group("cmd2")
         set_id = int(match.group("set_id"), 16)
         status_ok = match.group("status") == "OK"
@@ -171,24 +195,20 @@ def parse_response(reponse:bytearray) -> Response | None:
 
         return Response(cmd2, set_id, status_ok, data0)
     except Exception as e:
-        logging.error("Could not parse data from %s", reponse)
+        logger.error("Could not parse data from %s" % reponse)
         raise e
 
 
 class LgTv:
     """Control an LG TV with serial port."""
 
-    def __init__(
-        self,
-        serial_url,
-        set_id=0
-    ) -> None:
+    def __init__(self, serial_url, set_id=0) -> None:
         self._serial_url = serial_url
         self._set_id = set_id
         self._lock = asyncio.Lock()
-        self._reader:asyncio.StreamReader
-        self._writer:asyncio.StreamWriter
         self._on_disconnect = None
+        self._reader: asyncio.StreamReader
+        self._writer: asyncio.StreamWriter
 
     async def __aenter__(self):
         return self
@@ -198,7 +218,9 @@ class LgTv:
 
     async def connect(self, on_disconnect=None):
         try:
-            (self._reader, self._writer) = await serial_asyncio.open_serial_connection(url=self._serial_url, baudrate=9600)
+            (self._reader, self._writer) = await serial_asyncio.open_serial_connection(
+                url=self._serial_url, baudrate=9600
+            )
             self._on_disconnect = on_disconnect
         except SerialException:
             raise ConnectionError("Could not connect to LG TV")
@@ -208,210 +230,213 @@ class LgTv:
             self._writer.close()
             await self._writer.wait_closed()
 
-
-    async def _do_command(self, command1, command2, data0:int, data1:int|None = None, data2:int|None = None, data3:int|None = None, data4:int|None = None, data5:int|None = None) -> Response|bool:
+    async def _do_command(
+        self,
+        command1,
+        command2,
+        data0: int,
+        data1: int | None = None,
+        data2: int | None = None,
+        data3: int | None = None,
+        data4: int | None = None,
+        data5: int | None = None,
+    ) -> Response | bool:
         async with self._lock:
-            command = build_command(command1, command2, self._set_id, data0, data1, data2, data3, data4, data5)
-            print("before send", command)
+            command = build_command(
+                command1,
+                command2,
+                self._set_id,
+                data0,
+                data1,
+                data2,
+                data3,
+                data4,
+                data5,
+            )
             self._writer.write(command)
 
-            print("before await")
             try:
                 async with asyncio.timeout(5):
                     command = bytearray()
                     while True:
                         data = await self._reader.read(1)
                         if data == END_MARKER:
-                            print("parsing data:", command)
+                            logger.debug("parsing data: %s" % command)
                             result = parse_response(command)
-                            return result
-                        if data == WEIRD_VALUE:
+                            return result or False
+                        if data == 0xFF:
                             # Sometimes the TV returns 0xFF, it is unclear why
                             # and the value is not documented
-                            # I think it means something like "busy"
+                            # Seems to mean something like "busy"?
                             return False
-                        if data == b'':
-                            # Connection closed
-                            raise ConnectionError("Connection closed")
+                        if data == b"":
+                            raise ConnectionError("Connection lost")
                         command.extend(data)
             except TimeoutError:
-                logger.debug("TimeoutError")
+                logger.debug("Timeout while waiting for response")
             except ConnectionError:
                 if self._on_disconnect:
                     await self._on_disconnect()
 
-            print("_do_command failed")
             return False
-            # return await self._protocol.do_command(command1, command2, self._set_id, data0, data1, data2, data3, data4, data5)
 
-
-    async def set_power_on(self, value:bool) -> None:
+    async def set_power_on(self, value: bool) -> None:
         await self._do_command("k", "a", 1 if value else 0)
 
-    async def get_power_on(self) -> bool|None:
+    async def get_power_on(self) -> bool | None:
         response = await self._do_command("k", "a", 0xFF)
         if isinstance(response, Response) and response.status_ok:
             return response.data0 == 1
         return None
 
-
-    async def set_mute(self, mute:bool) -> None:
+    async def set_mute(self, mute: bool) -> None:
         await self._do_command("k", "e", 1 if mute else 0)
 
-    async def get_mute(self) -> bool|None:
+    async def get_mute(self) -> bool | None:
         response = await self._do_command("k", "e", 0, 0xFF)
         if isinstance(response, Response) and response.status_ok:
             return response.data0 == 1
         return None
-    
 
-    async def set_volume(self, value:int) -> None:
+    async def set_volume(self, value: int) -> None:
         assert value >= 0
         assert value <= 100
-        response = await self._do_command("k", "f", value)
+        await self._do_command("k", "f", value)
 
-    async def get_volume(self) -> int|None:
+    async def get_volume(self) -> int | None:
         response = await self._do_command("k", "f", 0xFF)
         if isinstance(response, Response) and response.status_ok:
             return response.data0
         return None
 
     async def volume_up(self) -> None:
-        response = await self._do_command("m", "c", 2)
+        await self._do_command("m", "c", 2)
 
     async def volume_down(self) -> None:
-        response = await self._do_command("m", "c", 3)
-
+        await self._do_command("m", "c", 3)
 
     async def channel_up(self) -> None:
-        response = await self._do_command("m", "c", 0)
+        await self._do_command("m", "c", 0)
 
     async def channel_down(self) -> None:
-        response = await self._do_command("m", "c", 1)
+        await self._do_command("m", "c", 1)
 
-
-    async def remote_key(self, code:RemoteKeyCode) -> None:
+    async def remote_key(self, code: RemoteKeyCode) -> None:
         """Allows sending remote key codes, note that some key codes already have methods like `volume_up`"""
         await self._do_command("m", "c", code)
 
-
-    async def set_contrast(self, value:int) -> None:
+    async def set_contrast(self, value: int) -> None:
         assert value >= 0
         assert value <= 100
-        response = await self._do_command("k", "g", value)
+        await self._do_command("k", "g", value)
 
-    async def get_contrast(self) -> int|None:
+    async def get_contrast(self) -> int | None:
         response = await self._do_command("k", "g", 0xFF)
         if isinstance(response, Response) and response.status_ok:
             return response.data0
         return None
 
-
-    async def set_brightness(self, value:int) -> None:
+    async def set_brightness(self, value: int) -> None:
         assert value >= 0
         assert value <= 100
-        response = await self._do_command("k", "h", value)
+        await self._do_command("k", "h", value)
 
-    async def get_brightness(self) -> int|None:
+    async def get_brightness(self) -> int | None:
         response = await self._do_command("k", "h", 0xFF)
         if isinstance(response, Response) and response.status_ok:
             return response.data0
         return None
 
-
-    async def set_color(self, value:int) -> None:
+    async def set_color(self, value: int) -> None:
         assert value >= 0
         assert value <= 100
-        response = await self._do_command("k", "i", value)
+        await self._do_command("k", "i", value)
 
-    async def get_color(self) -> int|None:
+    async def get_color(self) -> int | None:
         response = await self._do_command("k", "i", 0xFF)
         if isinstance(response, Response) and response.status_ok:
             return response.data0
         return None
 
-
-    async def set_sharpness(self, value:int) -> None:
+    async def set_sharpness(self, value: int) -> None:
         assert value >= 0
         assert value <= 100
-        response = await self._do_command("k", "k", value)
+        await self._do_command("k", "k", value)
 
-    async def get_sharpness(self) -> int|None:
+    async def get_sharpness(self) -> int | None:
         response = await self._do_command("k", "k", 0xFF)
         if isinstance(response, Response) and response.status_ok:
             return response.data0
         return None
 
+    async def set_remote_control_lock(self, value: bool) -> None:
+        await self._do_command("k", "m", 1 if value else 0)
 
-    async def set_remote_control_lock(self, value:bool) -> None:
-        response = await self._do_command("k", "m", 1 if value else 0)
-
-    async def get_remote_control_lock(self) -> bool|None:
+    async def get_remote_control_lock(self) -> bool | None:
         response = await self._do_command("k", "m", 0xFF)
         if isinstance(response, Response) and response.status_ok:
             return response.data0 == 1
         return None
 
-
-    async def set_treble(self, value:int) -> None:
+    async def set_treble(self, value: int) -> None:
         assert value >= 0
         assert value <= 100
-        response = await self._do_command("k", "r", value)
+        await self._do_command("k", "r", value)
 
-    async def get_treble(self) -> int|None:
+    async def get_treble(self) -> int | None:
         response = await self._do_command("k", "r", 0xFF)
         if isinstance(response, Response) and response.status_ok:
             return response.data0
         return None
 
-
-    async def set_bass(self, value:int) -> None:
+    async def set_bass(self, value: int) -> None:
         assert value >= 0
         assert value <= 100
-        response = await self._do_command("k", "s", value)
+        await self._do_command("k", "s", value)
 
-    async def get_bass(self) -> int|None:
+    async def get_bass(self) -> int | None:
         response = await self._do_command("k", "s", 0xFF)
         if isinstance(response, Response) and response.status_ok:
             return response.data0
         return None
 
-
-    async def set_balance(self, value:int) -> None:
+    async def set_balance(self, value: int) -> None:
         assert value >= 0
         assert value <= 100
-        response = await self._do_command("k", "t", value)
+        await self._do_command("k", "t", value)
 
-    async def get_balance(self) -> int|None:
+    async def get_balance(self) -> int | None:
         response = await self._do_command("k", "t", 0xFF)
         if isinstance(response, Response) and response.status_ok:
             return response.data0
         return None
 
-
-    async def set_color_temperature(self, value:int) -> None:
+    async def set_color_temperature(self, value: int) -> None:
         assert value >= 0
         assert value <= 100
-        response = await self._do_command("x", "u", value)
+        await self._do_command("x", "u", value)
 
-    async def get_color_temperature(self) -> int|None:
+    async def get_color_temperature(self) -> int | None:
         response = await self._do_command("x", "u", 0xFF)
         if isinstance(response, Response) and response.status_ok:
             return response.data0
         return None
 
-
-    async def set_input(self, value:Input) -> None:
+    async def set_input(self, value: Input) -> None:
         await self._do_command("x", "b", value)
 
-    async def get_input(self) -> Input|None:
+    async def get_input(self) -> Input | None:
         response = await self._do_command("x", "b", 0xFF)
         if isinstance(response, Response) and response.status_ok:
             return Input(response.data0)
         return None
 
-    async def set_3d(self, mode:Mode3D, encoding:Encoding3D, right_to_left:bool, depth:int) -> None:
-        response = await self._do_command("x", "t", mode, encoding, 1 if right_to_left else 0, depth)
+    async def set_3d(
+        self, mode: Mode3D, encoding: Encoding3D, right_to_left: bool, depth: int
+    ) -> None:
+        await self._do_command(
+            "x", "t", mode, encoding, 1 if right_to_left else 0, depth
+        )
 
     # Does not seem to work even though my TV supports 3D
     # async def get_3d(self) -> Config3D:
@@ -419,7 +444,7 @@ class LgTv:
     #     return Config3D(Mode3D(response.data0), Encoding3D(response.data1), response.data2==1, response.data3)
 
 
-async def main(serial_url:str):
+async def main(serial_url: str):
     async with LgTv(serial_url) as tv:
         await tv.connect()
 
@@ -440,6 +465,7 @@ async def main(serial_url:str):
         print(f"{await tv.get_color_temperature()=}")
         print(f"{await tv.get_sharpness()=}")
         print(f"{await tv.get_remote_control_lock()=}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
