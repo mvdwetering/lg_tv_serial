def0561a7fea16a8705599dbb74cd4597f37ef2f
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2023-12-20 21:03:41 +0100

    Cleanup comments and formatting in __init__

--- custom_components/lg_tv_serial/__init__.py
+++ custom_components/lg_tv_serial/__init__.py
@@ -11,8 +11,6 @@ from .const import DOMAIN, LOGGER
 from .coordinator import LgTvCoordinator
 from .lgtv_api import LgTv
 
-# TODO List the platforms that you want to support.
-# For your initial PR, limit it to 1 platform.
 PLATFORMS: list[Platform] = [Platform.MEDIA_PLAYER]
 
 
@@ -20,10 +18,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up LG TV Serial from a config entry."""
 
     hass.data.setdefault(DOMAIN, {})
-    # TODO 1. Create API instance
+
     api = LgTv(entry.data["serial_url"])
 
-    # TODO 2. Validate the API connection (and authentication)
     async def on_disconnect():
         LOGGER.debug("Disconnected, attempt to reload integration")
         # Reload the entry on disconnect.
@@ -33,8 +30,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         except OperationNotAllowed:  # pragma: no cover
             # Can not reload when during setup
             # Which is fine, so just let it go
-            pass        
-        
+            pass
+
     try:
         await api.connect(on_disconnect)
     except ConnectionError as e:
@@ -42,10 +39,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     coordinator = LgTvCoordinator(hass, api)
     await coordinator.async_config_entry_first_refresh()
-
-
-    # TODO 3. Store an API object for your platforms to access
-    # hass.data[DOMAIN][entry.entry_id] = MyApi(...)
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
@@ -56,7 +49,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        coordinator:LgTvCoordinator = hass.data[DOMAIN].pop(entry.entry_id)
+        coordinator: LgTvCoordinator = hass.data[DOMAIN].pop(entry.entry_id)
         await coordinator.api.close()
 
     return unload_ok
