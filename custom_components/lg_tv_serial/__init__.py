caeb12c2b7aaee626f91bf5fc755d5a7812d41fe
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2025-11-01 18:41:53 +0100

    Remove target selector, add config entry selector

--- custom_components/lg_tv_serial/__init__.py
+++ custom_components/lg_tv_serial/__init__.py
@@ -14,6 +14,7 @@ from homeassistant.helpers.config_validation import make_entity_service_schema
 from .const import (
     ATTR_COMMAND_1,
     ATTR_COMMAND_2,
+    ATTR_CONFIG_ENTRY,
     ATTR_DATA_0,
     ATTR_DATA_1,
     ATTR_DATA_2,
@@ -45,70 +46,62 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
         Send raw command to the TV
         """
 
-        config_entry_ids = await async_extract_config_entry_ids(hass, call)
-        for config_entry_id in config_entry_ids:
-            # Need to check if it is our config entry since async_extract_config_entry_ids
-            # can return config entries from other integrations also
-            # (e.g. area id or devices with entities from multiple integrations)
-            if config_entry := hass.config_entries.async_get_entry(config_entry_id):
-                if (
-                    config_entry.domain == DOMAIN
-                    and config_entry.entry_id in hass.data[DOMAIN]
-                ):
+        if config_entry := hass.config_entries.async_get_entry(call.data.get(ATTR_CONFIG_ENTRY)):
 
-                    command1 = call.data.get(ATTR_COMMAND_1)
-                    if len(command1) != 1:
-                        raise ServiceValidationError(
-                            translation_domain=DOMAIN,
-                            translation_key="invalid_command_value",
-                            translation_placeholders={"command": ATTR_COMMAND_1, "wrong_value": command1},
-                        )
+            command1 = call.data.get(ATTR_COMMAND_1)
+            if len(command1) != 1:
+                raise ServiceValidationError(
+                    translation_domain=DOMAIN,
+                    translation_key="invalid_command_value",
+                    translation_placeholders={"command": ATTR_COMMAND_1, "wrong_value": command1},
+                )
 
-                    command2 = call.data.get(ATTR_COMMAND_2)
-                    if len(command2) != 1:
-                        raise ServiceValidationError(
-                            translation_domain=DOMAIN,
-                            translation_key="invalid_command_value",
-                            translation_placeholders={"command": ATTR_COMMAND_2, "wrong_value": command2},
-                        )
+            command2 = call.data.get(ATTR_COMMAND_2)
+            if len(command2) != 1:
+                raise ServiceValidationError(
+                    translation_domain=DOMAIN,
+                    translation_key="invalid_command_value",
+                    translation_placeholders={"command": ATTR_COMMAND_2, "wrong_value": command2},
+                )
 
-                    data: list[int | None] = []
-                    for attr in [
-                        ATTR_DATA_0,
-                        ATTR_DATA_1,
-                        ATTR_DATA_2,
-                        ATTR_DATA_3,
-                        ATTR_DATA_4,
-                        ATTR_DATA_5,
-                    ]:
-                        value = call.data.get(attr)
-                        if value is None:
-                            data.append(None)
+            data: list[int | None] = []
+            for attr in [
+                ATTR_DATA_0,
+                ATTR_DATA_1,
+                ATTR_DATA_2,
+                ATTR_DATA_3,
+                ATTR_DATA_4,
+                ATTR_DATA_5,
+            ]:
+                value = call.data.get(attr)
+                if value is None:
+                    data.append(None)
+                else:
+                    try:
+                        parsed_value = int(str(value).strip(), 0)  # 0 = auto base
+                        if 0 <= parsed_value <= 255:
+                            data.append(parsed_value)
                         else:
-                            try:
-                                parsed_value = int(str(value).strip(), 0)  # 0 = auto base
-                                if 0 <= parsed_value <= 255:
-                                    data.append(parsed_value)
-                                else:
-                                    raise ValueError
-                            except (TypeError, ValueError):
-                                raise ServiceValidationError(
-                                    translation_domain=DOMAIN,
-                                    translation_key="invalid_data_value",
-                                    translation_placeholders={"data_byte": attr, "wrong_value": value},
-                                )
+                            raise ValueError
+                    except (TypeError, ValueError):
+                        raise ServiceValidationError(
+                            translation_domain=DOMAIN,
+                            translation_key="invalid_data_value",
+                            translation_placeholders={"data_byte": attr, "wrong_value": value},
+                        )
 
-                    coordinator: LgTvCoordinator = hass.data[DOMAIN][
-                        config_entry.entry_id
-                    ]
-                    await coordinator.api.send_raw(command1, command2, data)
+            coordinator: LgTvCoordinator = hass.data[DOMAIN][
+                config_entry.entry_id
+            ]
+            await coordinator.api.send_raw(command1, command2, data)
 
     hass.services.async_register(
         DOMAIN,
         SERVICE_SEND_RAW,
         async_send_raw,
-        schema=make_entity_service_schema(
+        schema=vol.Schema(
             {
+                vol.Required(ATTR_CONFIG_ENTRY): cv.string,
                 vol.Required(ATTR_COMMAND_1): cv.string,
                 vol.Required(ATTR_COMMAND_2): cv.string,
                 vol.Required(ATTR_DATA_0): cv.string,
